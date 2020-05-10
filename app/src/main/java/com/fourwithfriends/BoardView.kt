02a12ee15eb66a8e7ee7ab784221ead0bcb3fd7c
Aaron Pradhan <apradhan@tripadvisor.com>
2020-05-10 12:15:27 -0400

    Cleaning up style and code

--- app/src/main/java/com/fourwithfriends/BoardView.kt
+++ app/src/main/java/com/fourwithfriends/BoardView.kt
@@ -14,19 +14,19 @@ import java.util.*
 
 const val TOKEN_DIAMETER = 0.95
 const val SCREEN_WIDTH = 0.9
-const val TOP_PADDING = 0.05
 
 class BoardView(context: Context, attrs: AttributeSet) : View(context, attrs) {
 
-    private val rect: Rect = Rect(0, 0, 100, 100)
-//    private val paint: Paint = Paint()
     private val blue: Paint = Paint()
+    private val boardBgPaint = Paint()
     private val orange: Paint = Paint()
     private val white: Paint = Paint()
+    private val bgColor: Int = Color.rgb(105, 88, 41)
 
     init {
         blue.color = Color.CYAN
-        orange.color = Color.rgb(255, 128, 0)
+        boardBgPaint.color = Color.rgb(255, 128, 0)
+        orange.color = Color.rgb(255, 206, 0)
         white.color = Color.WHITE
     }
 
@@ -38,14 +38,10 @@ class BoardView(context: Context, attrs: AttributeSet) : View(context, attrs) {
         super.onDraw(canvas)
 
         val can: Canvas = canvas!!
-        can.drawColor(Color.rgb(105, 88, 41))
+        can.drawColor(bgColor)
+        can.drawRect(0F, 0F, width.toFloat(), (width * boardHeight / boardWidth).toFloat(), boardBgPaint)
 
-        model.setBoardCell(0, 0, PlayerColor.Blue)
         val board = model.board
-//        println("here is board ${Arrays.deepToString(board)}")
-        println("Width: $width")
-        println("Display width: ${getDisplayDimensions().widthPixels}")
-        can.drawRect(0F, 0F, 1440F, 1440F, orange)
         for (col in 0 until boardWidth) {
             for (row in 0 until boardHeight) {
                 val paint: Paint = when (board[col][row]!!) {
@@ -56,30 +52,28 @@ class BoardView(context: Context, attrs: AttributeSet) : View(context, attrs) {
                 can.drawOval(convertCoords(col, row), paint)
             }
         }
-        println("Converted 0 0: " + convertCoords(0, 0))
-        println("Converted 3 5: " + convertCoords(3, 5))
-//        can.drawOval(0F, 0F, 100F, 100F, blue)
     }
 
-    private fun getDisplayDimensions(): DisplayMetrics {
-        val displayMetrics = DisplayMetrics()
-        (context as Activity).windowManager
-            .defaultDisplay
-            .getMetrics(displayMetrics)
-        return displayMetrics
+    private fun convertRow(row: Int): Int {
+        return boardHeight - row - 1
     }
 
     private fun convertCoords(col: Int, row: Int): RectF {
         val boardWidthPx = width * SCREEN_WIDTH
+        val screenHeightAdjusted = width * boardHeight / boardWidth
+        val boardHeightPx = boardWidthPx * boardHeight / boardWidth
         val boardXOffsetPx = (width - boardWidthPx) / 2
-        val boardYOffsetPx = height * TOP_PADDING
+        val boardYOffsetPx = (screenHeightAdjusted - boardHeightPx) / 2
         val cellWidthPx = boardWidthPx / boardWidth
-        println("Cell width: $cellWidthPx")
+        val tokenWidthPx = cellWidthPx * TOKEN_DIAMETER
+        val tokenOffsetPx = (cellWidthPx - tokenWidthPx) / 2
+        val convertedRow = convertRow(row)
+
         return RectF(
-            (boardXOffsetPx + col * cellWidthPx).toFloat(),
-            (boardYOffsetPx + row * cellWidthPx).toFloat(),
-            (boardXOffsetPx + (col + 1) * cellWidthPx).toFloat(),
-            (boardYOffsetPx + (row + 1) * cellWidthPx).toFloat()
+            (boardXOffsetPx + col * cellWidthPx + tokenOffsetPx).toFloat(),
+            (boardYOffsetPx + convertedRow * cellWidthPx + tokenOffsetPx).toFloat(),
+            (boardXOffsetPx + (col + 1) * cellWidthPx - tokenOffsetPx).toFloat(),
+            (boardYOffsetPx + (convertedRow + 1) * cellWidthPx - tokenOffsetPx).toFloat()
         )
     }
 }
